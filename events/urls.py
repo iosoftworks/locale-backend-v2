a9b8c988279f7274b6aef95b21ce742a15bf09ec
Mark Lynskey <mlynskey2@gmail.com>
2019-09-08 07:52:58 +1200

    Added CRUD for events

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import EventListView, EventDetailView
+from .views import EventListView, EventDetailView, EventCreateView, EventUpdateView, EventDeleteView
 
 urlpatterns = [
+    
+    path('event/new/', EventCreateView.as_view(), name='event_new'),
+    path('event/<int:pk>/edit/', EventUpdateView.as_view(), name='event_edit'),
+    path('event/<int:pk>/delete/', EventDeleteView.as_view(), name='event_delete'),
     path('event/<int:pk>/', EventDetailView.as_view(), name='event_detail'),
     path('', EventListView.as_view(), name='home'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Event
 
@@ -11,3 +13,18 @@ class EventListView(ListView):
 class EventDetailView(DetailView):
     model = Event
     template_name = 'event_detail.html'
+
+class EventCreateView(CreateView):
+    model = Event
+    template_name = 'event_new.html'
+    fields = ['day', 'title', 'description', 'start_time', 'end_time', 'location', 'price']
+
+class EventUpdateView(UpdateView):
+    model = Event
+    template_name = 'event_edit.html'
+    fields = ['day', 'title', 'description', 'start_time', 'end_time', 'location', 'price']
+
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = 'event_delete.html'
+    success_url = reverse_lazy('home')
